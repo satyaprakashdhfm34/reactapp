51d7b4bd05cca30e2e48bbbc853a29326106d218
satyaprakashdhfm34 <159006547+satyaprakashdhfm34@users.noreply.github.com>
2024-02-05 15:52:04 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Your Name</h1>
+        <p>Web Developer</p>
+        <p>Welcome to my profile website!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
